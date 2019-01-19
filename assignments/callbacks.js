4bfcafdaaace6c1ea817a9c9c95a3c421050f8b3
raberin <rmvaberin@gmail.com>
2019-01-18 23:15:06 -0800

    Edit:: Last push was an incomplete saved callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -60,12 +60,24 @@ multiplyNums(5,3, (product) => {
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  if(list.includes(item)){
+    return cb(true);
+  } else {
+    return false;
+  }
 }
+
+contains('booty', items, (trueOrFalse) => {
+  console.log(trueOrFalse);
+});
 
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
+  let result = [];
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  
+
 }
